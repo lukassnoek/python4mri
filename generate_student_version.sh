4ac898d7074b282c16e4fb4b28e5639cabfe8211
Lukas Snoek <lukassnoek@gmail.com>
2019-09-12 12:04:12 +0200

    Initial commit

---
+++ generate_student_version.sh
@@ -0,0 +1,3 @@
+# Ignore this file; only used to generate a 'student version' of the notebook
+nbgrader generate_assignment -f --assignment python4mri --notebook python4mri --course-dir .
+cp source/python4mri/* .
